6e509949d7a35127ecfea553e29b568c606692af
J4g0n <simon.andreux@gmail.com>
2015-11-18 17:33:44 +0100

    remove .idea files

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,12 +1,18 @@
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.phantomjs.{PhantomJSDriverService, PhantomJSDriver}
+import org.openqa.selenium.remote.{CapabilityType, DesiredCapabilities}
 import org.scalatest.selenium.WebBrowser
 import org.scalatest.{FlatSpec, Matchers}
 
 
 class Test extends FlatSpec with Matchers with WebBrowser {
 
-  implicit val webDriver: WebDriver = new PhantomJSDriver
+  val baseCaps = new DesiredCapabilities
+  baseCaps.setCapability(CapabilityType.SUPPORTS_FINDING_BY_CSS, true)
+  baseCaps.setCapability("takesScreenshot", true)
+  baseCaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/local/bin/phantomjs")
+
+  implicit val webDriver: WebDriver = new PhantomJSDriver(baseCaps)
   go to "http://www.amazon.com"
   click on "twotabsearchtextbox"
   textField("twotabsearchtextbox").value = "Scala"
