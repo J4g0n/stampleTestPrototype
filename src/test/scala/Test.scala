499905bdd5778baf575ac9088d990c927d7309d2
J4g0n <simon.andreux@gmail.com>
2015-11-20 18:21:02 +0100

    test some part of on boarding + set implicitly wait when loading components

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,4 +1,5 @@
 import java.net.URL
+import java.util.concurrent.TimeUnit
 
 import org.openqa.selenium.{By, WebDriver}
 import org.openqa.selenium.phantomjs.{PhantomJSDriver, PhantomJSDriverService}
@@ -24,6 +25,7 @@ class Test extends FlatSpec with Matchers with WebBrowser {
 
   //webDriver.manage().window().setSize(new org.openqa.selenium.Dimension(1920, 1080))
   webDriver.manage().window().maximize()
+  webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
   go to "http://staging.stample.co/login"
   println(webDriver.manage().window().getSize)
 
@@ -42,29 +44,39 @@ class Test extends FlatSpec with Matchers with WebBrowser {
   }
 
   "User" should "be able to sign up to Stample" in {
-    Thread.sleep(1000)
+    val number = (Math.random() * 1000000).toInt.toString
     click on cssSelector("li.main-nav.cd-signup a")
 
     click on id("signup-email")
-    emailField(id("signup-email")).value = "quelquun@stample.co"
+    emailField(id("signup-email")).value = s"quelquun$number@stample.co"
     click on id("signup-username")
-    textField(id("signup-username")).value = "JoDalton"
+    textField(id("signup-username")).value = s"JoDalton$number"
     click on id("signup-firstname")
     textField(id("signup-firstname")).value = "moins"
     click on id("signup-lastname")
     textField(id("signup-lastname")).value = "exunard"
     click on id("signup-password")
-    pwdField(id("signup-password")).value = "stample"
+    pwdField(id("signup-password")).value = "whatever"
     click on id("accept-terms")
     submit()
   }
 
-  /*"The amazon home page " should "have another this title" in {
+  "User" should "be able to do the onboarding" in {
+    click on cssSelector("#onboarding-welcome .onboarding-button")
+    click on cssSelector("#navigation .spaces-root-pane ul li:nth-child(1) .view-icon")
+    click on cssSelector("#navigation .create-space")
+    click on cssSelector("#category-creation #new-space-name")
+    textField(cssSelector("#category-creation #new-space-name")).value = "Blabla"
+    click on cssSelector("#category-creation .category-creation-button")
+    click on cssSelector("#category-creation .category-creation-button")
+  }
+
+  "The amazon home page " should "have another this title" in {
     go to "http://www.amazon.com"
     click on "twotabsearchtextbox"
     textField("twotabsearchtextbox").value  = "Scala"
     submit()
     pageTitle should be("Amazon.com: Scala")
     pageSource should include("Scala Cookbook: Recipes")
-  }*/
+  }
 }
