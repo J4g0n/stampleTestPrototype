41db53446285e5fd1b07f46afc5df98689587147
J4g0n <simon.andreux@gmail.com>
2016-01-18 11:03:12 +0100

    almost done but i need to finish it later

--- src/test/scala/testConfig/TestConfig.scala
+++ src/test/scala/testConfig/TestConfig.scala
@@ -9,29 +9,23 @@ import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.remote.{RemoteWebDriver, CapabilityType, DesiredCapabilities}
 
+import com.github.kxbmap.configs.syntax._
+import com.typesafe.config.ConfigFactory
+
 
 case class TestConfig (
-                        system: String = "osx",
-                        browser: String = "firefox",
-                        baseUrl: String = "localhost:9000",
-                        pathToChromeOSX: String = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-                        pathToFirefoxOSX: String = "/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox-bin",
-                        pathToChromeLinux: String = "/usr/bin/firefox",
-                        pathToFirefoxLinux: String = "/usr/bin/google-chrome",
-                        size: Option[(Int, Int)] = Some((1024, 728))
+                        browser: String,
+                        baseUrl: String,
+                        browserPath: String,
+                        dimension: Option[(Int, Int)]
                       )
-/*
-case class FirefoxConfigLinux extends TestConfig("linux", "firefox", _, "/usr/bin/firefox", )
-case class ChromeConfigLinux extends TestConfig()
-case class FirefoxConfigOSX extends TestConfig()
-case class ChromeConfigOSX extends TestConfig()
-*/
+
 
 /**
   * Created by dev on 20/11/15.
   */
 object TestConfig {
-  val testConfig: TestConfig = new TestConfig
+  val testConfig: TestConfig = loadConfig()
   val webDriver: WebDriver = createWebDriver
 
   def baseUrl = testConfig.baseUrl
@@ -51,11 +45,8 @@ object TestConfig {
   }
 
   private def createChromeDriver: WebDriver = {
-    System.setProperty("webdriver.chrome.driver", testConfig.pathToChromeOSX)
+    System.setProperty("webdriver.chrome.driver", testConfig.browserPath)
     val chromeCapabilities = DesiredCapabilities.chrome
-    // TODO useless atm because we can't click to trigger clipper see: http://stackoverflow.com/questions/25557533/open-a-chrome-extension-through-selenium-webdriver
-    // TODO there is still a hope to load it manually but i have not tried it yet
-    // val capsWithClipperExtension = addClipperExtension(chromeCapabilities)
 
     new RemoteWebDriver(new URL("http://localhost:9515"), chromeCapabilities)
   }
@@ -66,15 +57,12 @@ object TestConfig {
     baseCaps.setJavascriptEnabled(true)
     baseCaps.setCapability("takesScreenshot", true)
 
-    //baseCaps.setCapability("elementScrollBehavior", 1)
-    //baseCaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/local/bin/phantomjs")
-
     new RemoteWebDriver(new URL("http://localhost:4444"), baseCaps)
   }
 
   private def createFirefoxWebDriver: WebDriver = {
     val baseCaps = DesiredCapabilities.firefox
-    System.setProperty("webdriver.firefox.bin",testConfig.pathToFirefoxOSX)
+    System.setProperty("webdriver.firefox.bin",testConfig.browserPath)
 
     new FirefoxDriver(baseCaps)
   }
@@ -86,7 +74,7 @@ object TestConfig {
   }
 
   private def setFullscreen(webDriver: WebDriver): WebDriver = {
-    testConfig.size match {
+    testConfig.dimension match {
       case Some(dimensions) => webDriver.manage().window().setSize(new org.openqa.selenium.Dimension(dimensions._1, dimensions._2))
       case _ => webDriver.manage().window().maximize()
     }
@@ -98,12 +86,16 @@ object TestConfig {
     webDriver
   }
 
+  private def loadConfig(maybeConfigName: Option[String] = None): TestConfig = {
+    val configName = maybeConfigName.getOrElse("defaultSettings")
+    val config = ConfigFactory.load()
+    config.get[TestConfig](configName)
+  }
+
   private def createWebDriver: WebDriver = {
     val webDriver: WebDriver = testConfig match {
-      case TestConfig(_, "chrome", _, _, _, _, _, _) => createChromeDriver
-      case TestConfig(_, "firefox", _, _, _, _, _, _) => createFirefoxWebDriver
-      // case TestConfig("phantomjs", _, _, _, _, _, _) => createPhantomJSDriver
-      // case TestConfig("grid", _, _, _, _, _, _) => createSeleniumGridDriver // forget selenium grid for the moment
+      case TestConfig("chrome", _, _) => createChromeDriver
+      case TestConfig("firefox", _, _) => createFirefoxWebDriver
     }
     setMaxTimeoutBetweenActions(webDriver, 5)
     setFullscreen(webDriver)
